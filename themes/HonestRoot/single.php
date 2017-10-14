d32efed0b9dca4f58c45b477f1ad18dd0cbd13ef
Marie-Elise <marieelise.haddad10@gmail.com>
2017-10-14 18:38:02 +0200

    first version

---
+++ themes/HonestRoot/single.php
@@ -0,0 +1,41 @@
+<?php get_header(); //appel du template header.php  ?>
+
+<div id="content" class="container">
+  <div class="row">
+  </div>
+    <?php
+    // boucle WordPress
+    if (have_posts()){
+        while (have_posts()){
+            the_post();
+    ?>
+      <article class="col-sm-12">
+        <?php
+            if(has_pos_thumbnail())
+            {
+              echo '<div class="thumbnail">';
+                the_post_thumbnail("full");
+              echo '</div>';
+            }
+         ?>
+        <h1><?php the_title(); ?></h1>
+        <h2>Posté le <?php the_time('F jS, Y') ?></h2>
+        <p><?php the_content(); ?></p>
+      </article>
+
+    <?php
+    }
+    }
+    else {
+    ?>
+    Nous n'avons pas trouvé d'article répondant à votre recherche
+    <?php
+    }
+    ?>
+  </div>
+  <div class="pagination">
+    <?php wp_pagenavi(); ?>
+  </div>
+</div> <!-- /content -->
+
+<?php get_footer(); //appel du template footer.php ?>
