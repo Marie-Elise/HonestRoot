f4208cb27342d90ddcab36f4ee6a57255ebd30bb
Marie-Elise <marieelise.haddad10@gmail.com>
2017-11-03 11:17:09 +0100

    add content page concept

--- themes/HonestRoot/concept.php
+++ themes/HonestRoot/concept.php
@@ -2,17 +2,15 @@
 /*Template name: concept */
 
 get_header(); //appel du template header.php  ?>
-<div id="content" class="container">
-  <div class="row">
-    <h4 clas="col-sm-12">Décrouvrez la page concept !</h4>
-  </div>
-
+<div id="content" class="container-fluid">
   <?php
   $args=array(
-    'post_type' => 'smoothie',
-    'posts_per_page' => 6,
+    'post_type' => 'concept',
+    'posts_per_page' => 8,
     'orderby' => 'date',
-    'order'   => 'DESC',
+    'meta_key' => 'ordre',
+	  'orderby'	=> 'meta_value',
+	  'order'	=> 'ASC'
   );
 
   $the_query = new WP_Query( $args );
@@ -20,11 +18,33 @@ get_header(); //appel du template header.php  ?>
   if ( $the_query->have_posts() ) {
     while ( $the_query->have_posts() ) {
       $the_query->the_post();
+      $cat = wp_get_post_terms($post->ID, 'gamme', array("fields" => "all"));
       ?>
 
       <div class="row">
-        <h1 class="col-sm-12"></h1>
-      </div>
+        <div class="image-concept col-md-6 col-sm-6 col-xs-6">
+          <div class="<?= $cat[0]->slug ?>">
+            <div class="rectangle <?= $cat[0]->slug ?>"></div>
+            <div class="image-concept <?= $cat[0]->slug ?>">
+            <?php
+            if(has_post_thumbnail())
+            {
+              the_post_thumbnail("hub_concept_thumbnail");
+              echo '</div>';
+            }
+            ?>
+            </div>
+
+          </div>
+
+          <div class="text-concept col-md-6 col-sm-6 col-xs-6">
+            <h2 class="title <?= $cat[0]->slug ?>"><?php the_title(); ?></h2>
+            <div class="text <?= $cat[0]->slug ?>"><?php the_content(); ?></div>
+            <div class="rectanglesecond <?= $cat[0]->slug ?>"></div>
+            <h2 class="titlesecond <?= $cat[0]->slug ?>"><?php the_field('titlesecond');?></h2>
+            <div class="textsecond <?= $cat[0]->slug ?>"><?php the_field('textsecond');?></div>
+          </div>
+        </div>
 
       <?php
     }
@@ -34,8 +54,7 @@ get_header(); //appel du template header.php  ?>
   }
   /* Restore original Post Data */
   wp_reset_postdata();
-   ?>
- </div><!-- /content -->
-
+  ?>
+</div><!-- /content -->
 
 <?php get_footer(); //appel du template footer.php ?>
