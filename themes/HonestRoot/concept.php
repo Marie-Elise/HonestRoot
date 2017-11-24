7d545843cca4f8312265bfbbde6deb0d417ed650
Marie-Elise <marieelise.haddad10@gmail.com>
2017-11-24 19:14:44 +0100

    🥕 insertions of templates in the concept page

--- themes/HonestRoot/concept.php
+++ themes/HonestRoot/concept.php
@@ -10,7 +10,8 @@ get_header(); //appel du template header.php  ?>
     'orderby' => 'date',
     'meta_key' => 'ordre',
 	  'orderby'	=> 'meta_value',
-	  'order'	=> 'ASC'
+    'order'	=> 'ASC'
+    
   );
 
   $the_query = new WP_Query( $args );
@@ -18,33 +19,28 @@ get_header(); //appel du template header.php  ?>
   if ( $the_query->have_posts() ) {
     while ( $the_query->have_posts() ) {
       $the_query->the_post();
-      $cat = wp_get_post_terms($post->ID, 'gamme', array("fields" => "all"));
+      
       ?>
 
-      <div class="row">
-        <div class="image-concept col-md-6 col-sm-6 col-xs-6">
-          <div class="<?= $cat[0]->slug ?>">
-            <div class="rectangle <?= $cat[0]->slug ?>"></div>
-            <div class="image-concept <?= $cat[0]->slug ?>">
-            <?php
-            if(has_post_thumbnail())
-            {
-              the_post_thumbnail("hub_concept_thumbnail");
-              echo '</div>';
-            }
-            ?>
-            </div>
+      <div class=" concept">
 
-          </div>
+        <?php
+        if(get_field('display-type') === '1c-d') {
+          get_template_part('templates/concept-1c-d');
+        }
+        else if (get_field('display-type') === '1c-g') {
+          get_template_part('templates/concept-1c-g');
+        }
+        else if (get_field('display-type') === '2c') {
+           get_template_part('templates/concept-2c');
+        }
+        else if (get_field('display-type') === '1t') {
+          get_template_part('templates/concept-1t');
+        }
+        ?>
+        
 
-          <div class="text-concept col-md-6 col-sm-6 col-xs-6">
-            <h2 class="title <?= $cat[0]->slug ?>"><?php the_title(); ?></h2>
-            <div class="text <?= $cat[0]->slug ?>"><?php the_content(); ?></div>
-            <div class="rectanglesecond <?= $cat[0]->slug ?>"></div>
-            <h2 class="titlesecond <?= $cat[0]->slug ?>"><?php the_field('titlesecond');?></h2>
-            <div class="textsecond <?= $cat[0]->slug ?>"><?php the_field('textsecond');?></div>
-          </div>
-        </div>
+      </div>
 
       <?php
     }
