131ceedf874514880c2606213374b1eefa8a2fd6
Marie-Elise <marieelise.haddad10@gmail.com>
2017-11-24 19:05:22 +0100

    🎨 add part css

--- themes/HonestRoot/inc/styles_and_scripts.php
+++ themes/HonestRoot/inc/styles_and_scripts.php
@@ -5,6 +5,9 @@ function ajout_scripts() {
 // main.js
 wp_register_script('main_script', JS_URL.'/main.js', array('jquery'),'1.1', true);
 wp_enqueue_script('main_script');
+// recette.js
+wp_register_script('recette_script', JS_URL.'/recette.js', array('jquery'),'1.1', true);
+wp_enqueue_script('recette_script');
 //bootstrap
 wp_register_script('bootstrap_script', JS_URL.'/bootstrap.min.js', array('jquery'),'1.1', true);
 wp_enqueue_script('bootstrap_script');
@@ -13,7 +16,6 @@ wp_register_script('jquery_script', JS_URL.'/jquery.min.js', array('jquery'),'1.
 wp_enqueue_script('jquery_script');
 wp_register_script('owl_script', JS_URL.'/owl.carousel.min.js', array('jquery'),'1.1', true);
 wp_enqueue_script('owl_script');
-
 
 
 // FONTS
@@ -28,6 +30,21 @@ wp_enqueue_style( 'googlefont_style' );
 // main.css
 wp_register_style( 'main_style', CSS_URL.'/main.css' );
 wp_enqueue_style( 'main_style' );
+// concept.css
+wp_register_style( 'concept_style', CSS_URL.'/concept.css' );
+wp_enqueue_style( 'concept_style' );
+// produits.css
+wp_register_style( 'produits_style', CSS_URL.'/produits.css' );
+wp_enqueue_style( 'produits_style' );
+// single.css
+wp_register_style( 'single_style', CSS_URL.'/single.css' );
+wp_enqueue_style( 'single_style' );
+// contact.css
+wp_register_style( 'contact_style', CSS_URL.'/contact.css' );
+wp_enqueue_style( 'contact_style' );
+// home.css
+wp_register_style( 'home_style', CSS_URL.'/home.css' );
+wp_enqueue_style( 'home_style' );
 //bootstrap
 wp_register_style( 'bootstrap_style', CSS_URL.'/bootstrap.min.css' );
 wp_enqueue_style( 'bootstrap_style' );
