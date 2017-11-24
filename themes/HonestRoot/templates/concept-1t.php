9e3cbcb95f3bf191159339a30f0818ddff31cff8
Marie-Elise <marieelise.haddad10@gmail.com>
2017-11-24 19:12:55 +0100

    🥕 add  the template folder with the parts of the concept page

---
+++ themes/HonestRoot/templates/concept-1t.php
@@ -0,0 +1,29 @@
+<?php $cat = wp_get_post_terms($post->ID, 'gamme', array("fields" => "all")); ?>
+
+<div class="col-12">
+    <div class="row">
+        <div class="col-md-6 col-xs-12">
+            <div class="text-concept">
+                <h2 class="title <?= $cat[0]->slug ?>"><?php the_title(); ?></h2>
+                <div class="text <?= $cat[0]->slug ?>"><?php the_content(); ?></div>
+                <div class="rectanglesecond <?= $cat[0]->slug ?>"></div>
+                <h2 class="titlesecond <?= $cat[0]->slug ?>"><?php the_field('titlesecond');?></h2>
+                <div class="textsecond <?= $cat[0]->slug ?>"><?php the_field('textsecond');?></div>
+            </div>
+        </div>
+        
+        <div class="col-12 col-md-6 sol-sm-4 col-xs-6">
+            <div class="rectangle <?= $cat[0]->slug ?>">
+            <div class="image-concept <?= $cat[0]->slug ?>">
+            <?php
+                if(has_post_thumbnail())
+                {
+                    the_post_thumbnail("hub_concept_thumbnail");
+                    echo '</div>';
+                 }
+                ?>
+              </div>
+            </div>
+        </div>
+    </div>
+</div>
