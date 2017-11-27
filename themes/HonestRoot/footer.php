089853304c86405d5243a9fbc250e3d7840408ca
Marie-Elise <marieelise.haddad10@gmail.com>
2017-11-27 11:41:24 +0100

    add page recette

--- themes/HonestRoot/footer.php
+++ themes/HonestRoot/footer.php
@@ -1,7 +1,56 @@
 <footer id="footer">
-<!--
-    Tout le contenu de la partie head de mon site
- -->
+<div class="container">
+<div class="row">
+         <div class="col-lg-5 col-md-5 col-sm-4 col-xs-12">
+             <ul class="adress">
+                  <span>Découvrez-nous, suivez nous, échangeons, partageons ensemble autour de votre smoothie !</span>
+                   <li>
+                     <p>contact@honestroot.fr</p>
+                   </li>
+                   <li>
+                     <p>Mentions légales : HonestRoot Smoothie, based in UK</p>
+                   </li>
+              </ul>
+         </div>
+         <div class="col-lg-4 col-md-4 col-sm-4 col-xs-12">
+             <ul class="contact">
+                  <span>Page</span>    
+                  <li>
+                     <a href="page-home">Accueil</a>
+                   </li>    
+                   <li>
+                      <a href="concept">Concept</a>
+                   </li>   
+                   <li>
+                     <a href="produits">Produits</a>
+                   </li> 
+                   <li>
+                     <a href="recettes">Recettes</a>
+                   </li> 
+                   <li>
+                      <a href="contact">Contact</a>
+                   </li>
+             </ul>
+         </div>
+         <div class="col-lg-3 col-md-3 col-sm-4 col-xs-12">
+            <ul class="social">
+                       <span>Nous suivre</span>    
+                        <li>
+                             <a href="#"><i class="fa fa-facebook fa-2x"></i></a>
+                        </li>
+                        <li>
+                             <a href="#"><i class="fa fa-twitter fa-2x"></i></a>
+                        </li>
+                        <li>
+                             <a href="https://www.instagram.com/honestroot/"><i class="fa fa-instagram fa-2x"></i></a>
+                        </li>
+                        <li>
+                             <a href="#"><i class="fa fa-google-plus fa-2x"></i></a>
+                       </li> 
+              </ul>
+         </div>
+    </div> 
+ </div>
 </footer>
 
 <!-- Execution de la fonction wp_footer() obligatoire ! -->
